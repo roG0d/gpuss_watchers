0040d583bfdf6b7cadf9fa4f2eb04e89cede9d8d
rog0d <rodgarcas98@gmail.com>
2024-12-27 00:06:49 +0000

    Fixed: basic_use_dpa_transformer_engine

--- benchmarks/transformer_engine_basic_use.py
+++ benchmarks/transformer_engine_basic_use.py
@@ -20,20 +20,23 @@ ckpt_attn = False
 # workspace optimization path for cuDNN attention
 workspace_opt = True
 # QKV memory layout
-qkv_layout = "bshd_bshd_bshd"
+qkv_layout = "bshd"
 # padding between sequences for qkv_format=thd
 pad_between_seqs = False
 # training mode
 is_training = True
-"""
-model_configs = {
-    #   test: batch_size, heads, num_gqa_heads, head_dim_qk, max_seqlen_q, max_seqlen_kv, dropout_p, mask, bias
-    "test_0": ModelConfig(2, 16, 16, 64, 512, 512, 0.0, "no_mask", "no_bias"),  # short seq
-    "test_1": ModelConfig(2, 16, 16, 128, 2048, 2048, 0.0, "causal", "no_bias"),  # longer seq, mask
-    "test_2": ModelConfig(2, 16, 16, 128, 2048, 2048, 0.0, "causal", "post_scale_bias"),  # bias
-    "test_3": ModelConfig(2, 32, 4, 128, 8192, 8192, 0.0, "causal", "no_bias"),  # GQA
-}
-"""
+
+# Set dimensions.
+seq_len = 128
+b_size = 1
+emb_size = 512
+num_heads = 8
+
+
+rand_query = torch.randn(b_size, seq_len, num_heads, int(emb_size/num_heads), device="cuda")
+rand_key = torch.randn(b_size, seq_len, num_heads, int(emb_size/num_heads), device="cuda")
+rand_value = torch.randn(b_size, seq_len, num_heads, int(emb_size/num_heads), device="cuda")
+
 
 # Create an FP8 recipe. Note: All input args are optional.
 fp8_recipe = recipe.DelayedScaling(margin=0, fp8_format=recipe.Format.E4M3)
@@ -50,13 +53,12 @@ for i in range(num_iters):
     with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe):
         # Is this an op of dpa or a block of dpa?
         te.DotProductAttention(
-            num_attention_heads=16,
-            kv_channels=128,
-            num_gqa_groups=16,
+            num_attention_heads=8,
+            kv_channels= int(emb_size/num_heads),
             attention_dropout=0.0,
             qkv_format=qkv_layout,
             attn_mask_type="causal"
-        )
+        ).forward(query_layer=rand_query, key_layer=rand_key, value_layer=rand_value)
     
 torch.cuda.synchronize()
 dpa_total_time = (time.time() - dpa_start) * 1e3
